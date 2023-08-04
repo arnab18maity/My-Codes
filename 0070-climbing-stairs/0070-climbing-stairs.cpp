705dad5c95e557d8126c7db058aeca0daa872b2a
arnab18maity <122111430+arnab18maity@users.noreply.github.com>
2023-08-04 23:44:44 +0530

    Time: 0 ms (100.00%), Space: 6 MB (57.56%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     int solve(int n,vector<int> &dp){
-        if(n == 0){
-            return 1;
-        }
+        
         
         if(n == 1){
             return 1;
@@ -19,7 +17,19 @@ public:
     }
     
     int climbStairs(int n) {       
-      vector<int>dp(n+1,-1);        
-      return solve(n,dp);
+      if(n == 0){
+            return 1;
+        }
+        
+       int curr = 1;
+       int prv  = 1;
+       int ans  = 1;
+       for(int i = 2; i <= n ; i++){
+           ans = prv + curr;
+           prv = curr;
+           curr = ans;
+       }
+        
+      return ans;
     }
 };
