2e7ae711af01304e9f0f5a6ec36c8bb18aa758dd
arnab18maity <122111430+arnab18maity@users.noreply.github.com>
2023-08-05 02:31:35 +0530

    Update 0070-climbing-stairs.cpp

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,21 +1,6 @@
 class Solution {
 public:
-    int solve(int n,vector<int> &dp){
-        
-        
-        if(n == 1){
-            return 1;
-        }
-        
-        if(dp[n] != -1){
-            return dp[n];
-        }
-        
-        return dp[n] = solve(n-1,dp) + solve(n-2,dp);
-        
-        
-    }
-    
+
     int climbStairs(int n) {       
       if(n == 0){
             return 1;
