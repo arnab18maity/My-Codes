a89adcbf00f20500f7968f146033e8fd7c91107a
arnab18maity <122111430+arnab18maity@users.noreply.github.com>
2023-08-10 00:39:17 +0530

    Time: 3 ms (99.31%), Space: 10 MB (71.78%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-/*
+/*  Memoization Solution
+
     int solve(int i,int j,vector<vector<int>> &grid,vector<vector<int>> &dp)
     {
          if(i == 0 && j == 0){
@@ -18,7 +19,10 @@ public:
         
         return dp[i][j] = min(up,left);
     }
-*/    
+*/ 
+
+/*  Tabulation Solution
+
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
@@ -41,5 +45,36 @@ public:
         }
         
         return dp[m-1][n-1];
+    }
+};
+
+*/
+    // Space Optimization
+
+int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        
+       vector<int>prev(n,0);
+        
+        for(int i = 0; i < m ; i++){
+            vector<int> curr(n,0);
+            for(int j = 0 ; j < n ; j++){
+               if(i == 0 && j == 0){
+                   curr[j] = grid[i][j];
+               } 
+               else{
+                 int up = 201,left = 201;
+                 if(i > 0) up = grid[i][j] + prev[j];
+                 if(j > 0) left = grid[i][j] + curr[j-1];
+                 
+                 curr[j] = min(up,left);
+               }
+            }
+            
+            prev = curr;
+        }
+        
+        return prev[n-1];
     }
 };
