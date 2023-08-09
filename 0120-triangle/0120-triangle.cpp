f78597dca7955913860fde7837a9e6b1071e8a08
arnab18maity <122111430+arnab18maity@users.noreply.github.com>
2023-08-10 01:41:18 +0530

    Time: 8 ms (52.50%), Space: 8.8 MB (67.90%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,5 +1,8 @@
 class Solution {
 public:
+  /*
+     Memoization Solution
+   
     int solve(int i,int j,int row,vector<vector<int>> &triangle,vector<vector<int>>& dp)
     {
         if(i == row) return triangle[i][j];
@@ -18,3 +21,136 @@ public:
         return solve(0,0,row-1,triangle,dp);
     }
 };
+
+*/
+
+/*
+     Tabulation Solution
+
+int minimumTotal(vector<vector<int>>& triangle) {
+        int row = triangle.size();
+        vector<vector<int>>dp(row,vector<int>(row,0));
+    
+        for(int j = 0; j < row; j++){
+            dp[row-1][j] = triangle[row-1][j];
+        }
+    
+        for(int i = row - 2; i >= 0; i--){
+            for(int j = i; j >= 0; j--){
+                  int down = triangle[i][j] + dp[i+1][j];
+                  int diagonal = triangle[i][j] + dp[i+1][j+1];
+                    
+                  dp[i][j] = min(down,diagonal);
+            }
+        }
+    
+        return dp[0][0];
+    }
+};
+
+*/
+   // Space Optimization  
+    
+ int minimumTotal(vector<vector<int>>& triangle) {
+        int row = triangle.size();
+        vector<int>prev(row,0);
+    
+        for(int j = 0; j < row; j++){
+            prev[j] = triangle[row-1][j];
+        }
+    
+        for(int i = row - 2; i >= 0; i--){
+             vector<int> curr(row,0);
+            for(int j = i; j >= 0; j--){
+                  int down = triangle[i][j] + prev[j];
+                  int diagonal = triangle[i][j] + prev[j+1];
+                    
+                  curr[j] = min(down,diagonal);
+            }
+            
+            prev = curr;
+        }
+    
+        return prev[0];
+    }
+};   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
