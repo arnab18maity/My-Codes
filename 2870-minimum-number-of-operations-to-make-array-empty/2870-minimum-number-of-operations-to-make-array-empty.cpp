3d76b6ef6d9ac94f05324be22c36f8d849c31b17
arnab18maity <122111430+arnab18maity@users.noreply.github.com>
2023-09-30 22:05:04 +0530

    Time: 130 ms (16.67%), Space: 84.3 MB (66.67%) - LeetHub

--- 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.cpp
+++ 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+/*
     int solve(int ind,vector<int> &nums, vector<int> &dp) {
         if(ind == nums.size()) return 0;
         if(dp[ind] != -1) return dp[ind];
@@ -29,6 +30,33 @@ public:
        if(ans >= 1e9) return -1;
         
        return ans;
+    }
+*/   
+    int minOperations(vector<int>& nums) {
+       sort(nums.begin(),nums.end());
+       int n = nums.size();
+       vector<int> dp(n,0); 
+       if(nums.size() == 0) return 0;
+        
+       for(int ind = n-1; ind >= 0; ind--){
+           int three = 1e9, two = 1e9;
+           if((nums.size() - (ind+1)) >= 2 && nums[ind] == nums[ind+1] && nums[ind] == nums[ind+2]) {
+             three = 1 + (ind + 3 < n ? dp[ind + 3] : 0);
+           }
+        
+           if((nums.size() - (ind+1)) >= 1 && nums[ind] == nums[ind+1]) {
+             two = 1 + (ind + 2 < n ? dp[ind + 2] : 0);
+          }
+        
+           if(ind != nums.size() - 1 && nums[ind] != nums[ind+1]) {
+              dp[ind] = 1e9;
+              continue;
+           }
+           
+           dp[ind] = min(two,three);
+       }
+        
+       return dp[0] >= 1e9 ? -1 : dp[0];
     }
 };
 
