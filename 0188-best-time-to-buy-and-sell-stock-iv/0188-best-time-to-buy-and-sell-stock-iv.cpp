50fd778ef721d718f8d56a8f9530ff75453de005
arnab18maity <122111430+arnab18maity@users.noreply.github.com>
2023-10-10 01:49:45 +0530

    Time: 12 ms (68.02%), Space: 14.1 MB (52.09%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,22 +1,38 @@
 class Solution {
 public:
-    int f(int ind,vector<int> &prices, int tranNo,int buy,vector<vector<int>>&dp) {
+ /*
+    int f(int ind,vector<int> &prices, int tranNo,vector<vector<int>>&dp) {
         if(ind == prices.size()) return 0;
         if(tranNo == 0) return 0;
         if(dp[ind][tranNo] != -1) return dp[ind][tranNo];
         
-        if(buy == 1) {
-            return dp[ind][tranNo] = max(-prices[ind] + f(ind+1,prices,tranNo-1,0,dp),
-                                      f(ind+1,prices,tranNo,1,dp));
+        if(tranNo % 2 == 0) {
+            return dp[ind][tranNo] = max(-prices[ind] + f(ind+1,prices,tranNo-1,dp),
+                                      f(ind+1,prices,tranNo,dp));
         }
         
-        return dp[ind][tranNo] = max(prices[ind] + f(ind+1,prices,tranNo-1,1,dp),
-                                  f(ind+1,prices,tranNo,0,dp));
+        return dp[ind][tranNo] = max(prices[ind] + f(ind+1,prices,tranNo-1,dp),
+                                  f(ind+1,prices,tranNo,dp));
     }
+*/
     
     int maxProfit(int k, vector<int>& prices) {
       int n = prices.size();
-      vector<vector<int>>dp(n,vector<int>(2*k+1,-1));
-      return f(0,prices,2*k,1,dp);
+      vector<vector<int>>dp(n+1,vector<int>(2*k+1,0));
+        
+       for(int ind = n-1; ind >= 0; ind--) {
+           for(int tranNo = 1; tranNo <= 2*k ; tranNo++) {
+               if(tranNo % 2 == 0) {
+                 dp[ind][tranNo] = max(-prices[ind] + dp[ind+1][tranNo-1],
+                                       dp[ind+1][tranNo]);
+               }
+               else{
+                dp[ind][tranNo] = max(prices[ind] + dp[ind+1][tranNo-1],
+                                      dp[ind+1][tranNo]); 
+              }
+           }
+       } 
+        
+      return dp[0][2*k];
     }
 };
