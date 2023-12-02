c147889042b581bcdf4328dd67312d0bb5db133f
arnab18maity <122111430+arnab18maity@users.noreply.github.com>
2023-12-03 02:03:34 +0530

    Time: 23 ms (21.46%), Space: 24.2 MB (92.91%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -4,11 +4,15 @@ public:
         int suffix = 1, prefix = 1;
         vector<int> ans(nums.size(), 1);
         
+        // Product of Array Except Self = Prefix_Product * Suffix_Product
+        
+        // Storing the prefix product while forward iteration
         for(int i = 0; i < nums.size(); i++) {
-            ans[i] *= prefix;
+            ans[i] = prefix;
             prefix *= nums[i];
         }
         
+        // Multiplying the suffix product while backward iteration
         for(int i = nums.size() - 1; i >= 0; i--) {
             ans[i] *= suffix;
             suffix *= nums[i];
