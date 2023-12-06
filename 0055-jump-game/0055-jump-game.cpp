19b815b87c8c363b864f287cb9c57a5d69ad2e78
arnab18maity <122111430+arnab18maity@users.noreply.github.com>
2023-12-07 00:42:08 +0530

    Time: 49 ms (56.27%), Space: 48.8 MB (63.17%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-   /*
+/*
     bool f(int ind, vector<int> &nums, vector<int> &dp) {
        if(ind == nums.size() - 1) return true;
        if(nums[ind] == 0) return false;
@@ -14,7 +14,7 @@ public:
        
         return dp[ind] = false;
     }
-   */ 
+  
     bool canJump(vector<int>& nums) {
       int n = nums.size();
       vector<int>dp(n,0);
@@ -32,4 +32,52 @@ public:
         
       return dp[0];
     }
+*/ 
+    
+/*
+ The idea is to iterate backwards, i.e., right to left, and keep track of the minimum jump length required, i.e., minJump, at a position.
+
+For the last index, the minimum jump length required would be zero because you have reached the last index. For each index or position that we go backwards(since we are iterating right to left), the minimum jump length required increases by 1.
+*/
+    bool canJump(vector<int>& nums) {
+        int minJump = 0;
+        for(int i = nums.size()-2; i >= 0; i--){
+            minJump++;
+            if(nums[i] >= minJump) minJump = 0;
+        }
+        
+       return minJump == 0;
+    }
+    
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
