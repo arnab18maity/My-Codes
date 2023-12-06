7aba18eace491924dd058496fbe90aea52f341c4
arnab18maity <122111430+arnab18maity@users.noreply.github.com>
2023-12-07 01:10:36 +0530

    Time: 83 ms (5.03%), Space: 8.9 MB (72.96%) - LeetHub

---
+++ 0274-h-index/0274-h-index.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool helper(int i, vector<int> &citations) {
+       int count = 0;
+       for(int ind = 0; ind < citations.size(); ind++) {
+           if(citations[ind] >= i) count++;
+       }
+        
+       return count >= i;
+    }
+    
+    int hIndex(vector<int>& citations) {
+       int ans =  0;
+       int low = 1, high = citations.size(); 
+       
+        while(low <= high) {
+          int mid = (low+high)/2;
+          
+          if(helper(mid,citations)) {
+             ans = max(ans,mid);
+             low++;
+          }
+          else{
+             high--;
+          }
+        }
+        
+        return ans;
+    }
+};
