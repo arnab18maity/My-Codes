9dcea01ca3de20e3b16440938c1182e1d15919da
Arnab Maity <122111430+arnab18maity@users.noreply.github.com>
2023-12-09 22:50:43 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (100.00%) - LeetHub

--- 2957-remove-adjacent-almost-equal-characters/2957-remove-adjacent-almost-equal-characters.cpp
+++ 2957-remove-adjacent-almost-equal-characters/2957-remove-adjacent-almost-equal-characters.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
   /*
     int f(int ind,string &word, vector<int> &dp) {
-       if(ind <= 0) return 0;
+       if(ind == 0) return 0;
         
-       char next = word[ind-1] + 1;
-       char prev = word[ind-1] - 1;
+       char next = word[ind-2] + 1;
+       char prev = word[ind-2] - 1;
         
        if(dp[ind] != -1) return dp[ind];
         
-       if(word[ind] != word[ind-1] && word[ind] != next && word[ind] != prev) {
+       if(word[ind-1] != word[ind-2] && word[ind-1] != next && word[ind-1] != prev) {
            return dp[ind] = 0 + f(ind-1,word,dp);
        }
         
@@ -20,6 +20,7 @@ public:
     int removeAlmostEqualCharacters(string word) {
        int n = word.size();
        int curr = 0, prev1 = 0, prev2 = 0;
+        
        for(int ind = 2; ind <= n; ind++) {
            char next = word[ind-2] + 1;
            char prev = word[ind-2] - 1;
