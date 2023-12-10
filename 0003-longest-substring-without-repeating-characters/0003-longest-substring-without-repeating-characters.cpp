7b91135324081bce72c9dc2d32c8a27bd44dbab2
Arnab Maity <122111430+arnab18maity@users.noreply.github.com>
2023-12-10 21:58:11 +0530

    Time: 17 ms (50.64%), Space: 9.5 MB (43.94%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -8,17 +8,15 @@ public:
         while(j < s.size()) {
           mp[s[j]]++;
           
-          if(mp.size() == j-i+1) {
-             maxLen = max(maxLen,j-i+1);
-          }
-          else{
+          if(mp.size() != j-i+1) {
              while(mp.size() != j-i+1) {
                  mp[s[i]]--;
                  if(mp[s[i]] == 0) mp.erase(s[i]);
                  i++;
              }
-            maxLen = max(maxLen,j-i+1); 
           }  
+        
+          maxLen = max(maxLen,j-i+1);   
           j++;          
         }
         
