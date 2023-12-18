7878e9ed3d19ecf703a5465b89ab50c772727b6d
Arnab Maity <122111430+arnab18maity@users.noreply.github.com>
2023-12-18 21:31:44 +0530

    Time: 51 ms (60.13%), Space: 42.1 MB (26.17%) - LeetHub

---
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    function toBe(val1) {
+      if(val1 === val) {
+        return true;
+      }
+      else{
+        throw new Error("Not Equal");
+      }
+    }
+    
+    function notToBe(val2) {
+      if(val2 !== val) {
+        return true;
+      }
+      else{
+        throw new Error("Equal");  
+      }
+    }
+    
+    return {
+      toBe : toBe,
+      notToBe : notToBe
+    };
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
