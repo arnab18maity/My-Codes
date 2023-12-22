cd19d651fd8cd47da496f2af0e1763ff3c59a202
Arnab Maity <122111430+arnab18maity@users.noreply.github.com>
2023-12-22 21:41:51 +0530

    Time: 14 ms (38.52%), Space: 16 MB (13.06%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+/*
     bool function(int ind, int space, string &s, unordered_map<string,int> &mp, vector<vector<int>> &dp) {
         if(ind == 0) {
           string str = s.substr(0,space - 0);
@@ -10,7 +11,9 @@ public:
         if(dp[ind][space] != -1) return dp[ind][space];
         
         string str = s.substr(ind,space-ind);
-        if(mp.find(str) !=  mp.end()) return dp[ind][space] = function(ind-1,ind,s,mp,dp) || function(ind-1,space,s,mp,dp);
+        
+        if(mp.find(str) !=  mp.end()) 
+            return dp[ind][space] = function(ind-1,ind,s,mp,dp) || function(ind-1,space,s,mp,dp);
         
         return dp[ind][space] = function(ind-1,space,s,mp,dp);
         
@@ -25,4 +28,50 @@ public:
       vector<vector<int>> dp(n,vector<int>(n+1,-1));  
       return function(n-1,n,s,mp,dp);
     }
+*/
+   bool wordBreak(string s, vector<string>& wordDict) {
+       unordered_map<string,int> mp;
+       int n = s.size();
+       for(auto it : wordDict) {
+         mp[it]++;  
+       }
+       
+      vector<vector<int>> dp(n,vector<int>(n+1,0)); 
+       
+      // Base Case
+      for(int space = 1; space <= n; space++) {
+         string str = s.substr(0,space - 0);
+          if(mp.find(str) !=  mp.end()) dp[0][space] = true;
+          else dp[0][space] = false;
+      }
+       
+      for(int ind = 1; ind < n; ind++) {
+          for(int space = ind + 1; space <= n; space++) {
+              string str = s.substr(ind,space-ind);
+        
+              if(mp.find(str) !=  mp.end()) 
+                 dp[ind][space] = dp[ind-1][ind] || dp[ind-1][space];
+
+             else dp[ind][space] = dp[ind-1][space]; 
+          }
+      }       
+       
+      return dp[n-1][n];
+    } 
+    
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
