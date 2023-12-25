26aa5668a567a970b75df366cd712a604bb7ef19
Arnab Maity <122111430+arnab18maity@users.noreply.github.com>
2023-12-25 22:08:53 +0530

    Time: 6 ms (24.73%), Space: 6.6 MB (71.45%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
     int myAtoi(string s) {
-       int len = s.size();
+        int len = s.size();
         double num = 0;
-        int i=0;
+        int i = 0;
+        
         while(s[i] == ' '){
             i++;
         }
-        bool positive = s[i] == '+';
-        bool negative = s[i] == '-';
-        positive == true ? i++ : i;
-        negative == true ? i++ : i;
+        
+        if(i == len) return 0;
+        
+        bool positive = (s[i] == '+');
+        bool negative = (s[i] == '-');
+        
+        if(positive == true || negative == true) i++;
+        
         while(i < len && s[i] >= '0' && s[i] <= '9'){
             num = num*10 + (s[i]-'0');
             i++;
         }
+        
         num = negative ? -num : num;
-        //cout<<num<<endl;
         num = (num > INT_MAX) ? INT_MAX : num;
         num = (num < INT_MIN) ? INT_MIN : num;
-        //cout<<num<<endl;
+        
         return int(num); 
     }
 };
