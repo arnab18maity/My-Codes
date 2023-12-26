0ccb27ee8dc54c0ebe9de4217f5528f1083961e6
Arnab Maity <122111430+arnab18maity@users.noreply.github.com>
2023-12-26 22:56:14 +0530

    Time: 6 ms (56.76%), Space: 6.8 MB (90.99%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.cpp
+++ 0043-multiply-strings/0043-multiply-strings.cpp
@@ -5,27 +5,20 @@ public:
        int n = num1.size(), m = num2.size();
         
        // Maximum size of the result of multiplication can be (n+m)
-       vector<int> res(n+m,0);
+       string res(n+m, '0');
         
        for(int i = m-1; i >= 0; i--) {
           for(int j = n-1; j >= 0; j--) {
-              res[i+j+1] += (num1[j] - '0') * (num2[i] - '0');
-              res[i+j] += res[i+j+1]/10;
-              res[i+j+1] %= 10;
+              int sum = (num1[j] - '0') * (num2[i] - '0') + (res[i+j+1] - '0');
+              res[i+j+1] = (sum % 10) + '0';
+              res[i+j] += sum/10;
           }
        }
-       
-       string ans;
-       int i = 0;
-        
+      
+       int i = 0;        
        // Remove if there exist any leading zeroes
-       while(res[i] == 0) i++;
-        
-       // Convert the remaining array to string 
-       while(i < n+m) {
-         ans += to_string(res[i++]); 
-       }
-        
-       return ans;
+       while(res[i] == '0') i++;
+    
+       return res.substr(i);
     }
 };
