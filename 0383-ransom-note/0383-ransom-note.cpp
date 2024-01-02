d05f9773df05da6a7850a707d804473568653a06
Arnab Maity <122111430+arnab18maity@users.noreply.github.com>
2024-01-02 21:33:00 +0530

    Time: 4 ms (98.92%), Space: 9.2 MB (34.21%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+       if(ransomNote.length() > magazine.length()) return false;
+        
+        int a[26] = {0};
+        
+        for(int i = 0; i < magazine.length(); i++){
+            char c = magazine[i];
+            a[c-'a']++;
+        }
+        
+        for(int i = 0; i < ransomNote.length(); i++){
+            char c = ransomNote[i];            
+            if(a[c-'a'] == 0){
+                return false;
+            }            
+            a[c-'a']--;
+        }
+        
+        return true; 
+    }
+};
