08dd87e84dc519050301e573b3c6b2eb7ce48744
Arnab Maity <122111430+arnab18maity@users.noreply.github.com>
2024-01-04 22:17:52 +0530

    Time: 119 ms (31.79%), Space: 61.5 MB (58.57%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,5 +1,32 @@
 class Solution {
 public:
+    int func(vector<int>& nums, int mid) {
+       int count = 0;
+       for(int i = 0; i < nums.size(); i++) {
+           if(nums[i] <= mid) count++;
+       }         
+       return count;
+    }
+    
+    int findDuplicate(vector<int>& nums) {
+       // Binary Search on Answer        
+       int low = 1;
+       int high = nums.size() - 1;
+       while(low <= high) {
+          int mid = (low+high)/2;
+          int cnt = func(nums,mid);
+          
+          if(cnt <= mid) {
+             low = mid + 1;
+          }
+          else{
+             high = mid - 1;
+          }
+       }
+        
+       return low;
+    }
+/*    
     int findDuplicate(vector<int>& nums) {
        int slow = nums[0];
        int fast = nums[nums[0]];
@@ -16,6 +43,43 @@ public:
          fast = nums[fast];
        }
         
-       return fast;
+       return slow; // We can return fast also
     }
+  */
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
