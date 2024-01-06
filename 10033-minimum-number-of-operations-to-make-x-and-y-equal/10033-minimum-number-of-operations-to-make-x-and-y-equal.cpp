329a7d5917f5f03adf0b5f7db00d99782454a3e3
Arnab Maity <122111430+arnab18maity@users.noreply.github.com>
2024-01-06 22:10:11 +0530

    Time: 7 ms (21.05%), Space: 10.2 MB (15.79%) - LeetHub

--- 10033-minimum-number-of-operations-to-make-x-and-y-equal/10033-minimum-number-of-operations-to-make-x-and-y-equal.cpp
+++ 10033-minimum-number-of-operations-to-make-x-and-y-equal/10033-minimum-number-of-operations-to-make-x-and-y-equal.cpp
@@ -10,10 +10,31 @@ private:
        int div5 = INT_MAX;
         
        if(x % 11 == 0) div11 =  1 + func(x/11, y,dp);
-       else div11 = min(x%11 + 1 + func(x/11, y,dp), (11 - x%11) + 1 + func(x/11 + 1, y,dp));
+       else {
+           // To get the previous multiple of 11 we have to decrement the number x % 11 times
+           // Eg. For 59 we have to decrement the number 59 % 11 = 4 times, then it will be 55
+           // Now we have to make the call for x/11 because it is the previous multiple of 11
+           // Eg. For 59 we have to make call for 59/11 = 5 because 55/11 = 5
+           // +1 for the current operation of (x/11)
+           int prevMultiple = x%11 + 1 + func(x/11, y,dp);
+           
+           // To get the next multiple of 11 we have to increment the number (11 - x%11) times
+           // Eg. For 59 we have to increment the number (11 - 59 % 11) = 7 times, then it will be 66
+           // Now we have to make the call for (x/11 + 1) because it is the next multiple of 11
+           // Eg. For 59 we have to make call for 59/11 + 1 = 6 because 66/11 = 6
+           // +1 for the current operation of (x/11 + 1)
+           int nextMultiple = (11 - x%11) + 1 + func(x/11 + 1, y,dp);
+           
+           // We have to store the minimum of both
+           div11 =  min(prevMultiple,nextMultiple);
+       }
         
        if(x % 5 == 0) div5 =  1 + func(x/5, y,dp); 
-       else div5 =  min(x%5 + 1 + func(x/5, y,dp), (5-x%5) + 1 + func(x/5 + 1, y,dp));
+       else {
+           int prevMultiple = x%5 + 1 + func(x/5, y,dp);
+           int nextMultiple = (5 - x%5) + 1 + func(x/5 + 1, y,dp);
+           div5 =  min(prevMultiple,nextMultiple);
+       }
         
        int dec1 = 1 + func(x-1, y,dp);
                
