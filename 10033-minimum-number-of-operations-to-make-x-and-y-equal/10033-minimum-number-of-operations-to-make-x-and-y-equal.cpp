1c491906035b53c718031f543390a837e152c2e6
Arnab Maity <122111430+arnab18maity@users.noreply.github.com>
2024-01-06 23:07:54 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (100.00%) - LeetHub

--- 10033-minimum-number-of-operations-to-make-x-and-y-equal/10033-minimum-number-of-operations-to-make-x-and-y-equal.cpp
+++ 10033-minimum-number-of-operations-to-make-x-and-y-equal/10033-minimum-number-of-operations-to-make-x-and-y-equal.cpp
@@ -1,5 +1,6 @@
 class Solution {
 private:
+/*
     int func(int x,int y, vector<int>& dp) {
        if(x == y) return 0;
        if(y > x) return y-x;
@@ -16,6 +17,7 @@ private:
            // Now we have to make the call for x/11 because it is the previous multiple of 11
            // Eg. For 59 we have to make call for 59/11 = 5 because 55/11 = 5
            // +1 for the current operation of (x/11)
+           
            int prevMultiple = x%11 + 1 + func(x/11, y,dp);
            
            // To get the next multiple of 11 we have to increment the number (11 - x%11) times
@@ -23,9 +25,11 @@ private:
            // Now we have to make the call for (x/11 + 1) because it is the next multiple of 11
            // Eg. For 59 we have to make call for 59/11 + 1 = 6 because 66/11 = 6
            // +1 for the current operation of (x/11 + 1)
+           
            int nextMultiple = (11 - x%11) + 1 + func(x/11 + 1, y,dp);
            
            // We have to store the minimum of both
+           
            div11 =  min(prevMultiple,nextMultiple);
        }
         
@@ -40,13 +44,58 @@ private:
                
        return dp[x] = min({div11,div5,dec1});
     }
-    
+*/    
 public:
-    int minimumOperationsToMakeEqual(int x, int y) {
+/*
+     int minimumOperationsToMakeEqual(int x, int y) {
         if(x == y) return 0;
         if(y > x) return y-x;
         
         vector<int> dp(x+1, -1);
         return func(x,y,dp);
-    }
+     }
+*/    
+    int minimumOperationsToMakeEqual(int a, int b) {
+        if(a == b) return 0;
+        if(b > a) return b-a;
+        
+        vector<int> dp(a+1, 0);
+        
+        for (int x = 0; x < b; x++) {
+            dp[x] = b - x;
+        }
+         
+        // dp[b] = 0; // Can ignore it because we are initializing everything with 0
+         
+        for(int x = b+1; x <= a; x++) {
+            int div11 = INT_MAX;
+            int div5 = INT_MAX;
+        
+           if(x % 11 == 0) div11 =  1 + dp[x/11];
+           else {
+               int prevMultiple = x%11 + 1 + dp[x/11];
+               int nextMultiple = (11 - x%11) + 1 + dp[x/11 + 1];
+               div11 =  min(prevMultiple,nextMultiple);
+           }
+
+           if(x % 5 == 0) div5 =  1 + dp[x/5]; 
+           else {
+               int prevMultiple = x%5 + 1 + dp[x/5];
+               int nextMultiple = (5 - x%5) + 1 + dp[x/5 + 1];
+               div5 =  min(prevMultiple,nextMultiple);
+           }
+
+           int dec1 = 1 + dp[x-1];
+
+           dp[x] = min({div11,div5,dec1});
+        }         
+         
+         return dp[a];
+     }
+    
 };
+
+
+
+
+
