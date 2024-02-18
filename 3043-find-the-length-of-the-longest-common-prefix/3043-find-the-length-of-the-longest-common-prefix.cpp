177f1cccf85b6b56cac2cfb9fc52e9f778590a24
Arnab Maity <122111430+arnab18maity@users.noreply.github.com>
2024-02-18 12:35:50 +0530

    Time: 236 ms (62.50%), Space: 134.2 MB (12.50%) - LeetHub

--- 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.cpp
+++ 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int length(int num) {
       if(num == 0) return 1;
+        
       int count = 0;
       while(num > 0) {
         num /= 10;
@@ -18,6 +19,7 @@ public:
         
        for(auto it : arr1) {
           while(it > 0) {
+            // For 1234 all the possible prefixes are 1234, 123, 12, 1
              st.insert(it);
              it /= 10;
           }
@@ -26,6 +28,8 @@ public:
        for(auto it : arr2) {
           while(it > 0) {
              if(st.find(it) != st.end()) {
+               // We need maximum, so if we get a bigger value no need to check for smaller ones
+              // Hence break...
                maxPrefix = max(maxPrefix, length(it));
                break;
              }
