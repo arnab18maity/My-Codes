07118ca1204d9cad5e907c5af71890050319397e
Arnab Maity <122111430+arnab18maity@users.noreply.github.com>
2024-03-04 00:17:13 +0530

    Time: 196 ms (30.00%), Space: 99.1 MB (20.00%) - LeetHub

--- 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.cpp
+++ 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int countSubmatrices(vector<vector<int>>& grid, int k) {
-       int m = grid.size();
+    /* int m = grid.size();
        int n = grid[0].size();
        int count = 0;
         
@@ -18,5 +18,56 @@ public:
        }
         
        return count;
-    }
+     */   
+        
+      // https://leetcode.com/problems/count-submatrices-with-top-left-element-and-sum-less-than-k/discuss/4814669/prefix-sum-2d-c/
+        
+       int m = grid.size();
+       int n = grid[0].size();
+       int count = 0;
+        
+       vector<vector<int>> pf(grid.begin(),grid.end());
+      
+       for(int i = 0; i < m; i++) {
+           for(int j = 0; j < n; j++) {
+             if(i >= 1) pf[i][j] += pf[i-1][j];
+             if(j >= 1) pf[i][j] += pf[i][j-1];
+             if(i >= 1 && j >= 1) pf[i][j] -= pf[i-1][j-1];
+             
+             if(pf[i][j] <= k) count++;
+             else break;
+           }
+       }
+        
+       return count;
+    }        
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
