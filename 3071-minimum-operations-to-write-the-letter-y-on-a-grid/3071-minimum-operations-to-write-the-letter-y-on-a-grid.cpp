46d7bf27cce4ee3eebd3a2a3d94136622a10622f
Arnab Maity <122111430+arnab18maity@users.noreply.github.com>
2024-03-06 03:45:03 +0530

    Time: 45 ms (83.78%), Space: 40.9 MB (98.56%) - LeetHub

--- 3071-minimum-operations-to-write-the-letter-y-on-a-grid/3071-minimum-operations-to-write-the-letter-y-on-a-grid.cpp
+++ 3071-minimum-operations-to-write-the-letter-y-on-a-grid/3071-minimum-operations-to-write-the-letter-y-on-a-grid.cpp
@@ -4,8 +4,8 @@ public:
         int n = grid.size();
         int threshold = n/2;
         
-        int zY = 0, oY = 0, tY = 0;
-        int znY = 0, onY = 0, tnY= 0;
+        int zY = 0, oY = 0, tY = 0; // Zero, One & twos which belong to Y
+        int znY = 0, onY = 0, tnY= 0; // Zero, One & twos which does not belong to Y
         
         for(int i = 0; i < n; i++) {
           for(int j = 0; j < n; j++) {
@@ -14,16 +14,19 @@ public:
                 else if(grid[i][j] == 1) oY++;
                 else tY++;
              }
+              
              else if(i <= threshold && j == n-i-1) {
                 if(grid[i][j] == 0) zY++;
                 else if(grid[i][j] == 1) oY++;
                 else tY++; 
              }
+              
              else if(i > threshold && j == threshold) {
                 if(grid[i][j] == 0) zY++;
                 else if(grid[i][j] == 1) oY++;
                 else tY++; 
              }
+              
              else{
                 if(grid[i][j] == 0) znY++;
                 else if(grid[i][j] == 1) onY++;
@@ -32,14 +35,14 @@ public:
           }
         }
       
-     int c1Zero = oY + tY + znY + tnY;
-     int c2Zero = oY + tY + znY + onY;
+     int c1Zero = oY + tY + znY + tnY; // (Y is 0, not Y is 1)
+     int c2Zero = oY + tY + znY + onY; // (Y is 0, not Y is 2)
          
-     int c1One = zY + tY + onY + tnY;
-     int c2One = zY + tY + onY + znY;
+     int c1One = zY + tY + onY + tnY; // (Y is 1, not Y is 0)
+     int c2One = zY + tY + onY + znY; // (Y is 1, not Y is 2)
          
-     int c1Two = oY + zY + znY + tnY;
-     int c2Two = oY + zY + onY + tnY;
+     int c1Two = oY + zY + onY + tnY; // (Y is 2, not Y is 0)
+     int c2Two = oY + zY + znY + tnY; // (Y is 2, not Y is 1)
          
      return min({c1Zero,c2Zero,c1One,c2One,c1Two,c2Two});
         
