b2cff5a48350261f566380e485b4edbbda73de4c
Arnab Maity <122111430+arnab18maity@users.noreply.github.com>
2024-05-14 22:28:11 +0530

    Time: 230 ms (38.16%), Space: 10.2 MB (28.03%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.cpp
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.cpp
@@ -4,11 +4,13 @@ public:
        if(i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() || grid[i][j] <= 0) return 0;
        
        int score = grid[i][j];
-       grid[i][j] = -grid[i][j]; // Making it negative to identify as visited
+       int temp = grid[i][j];
+        
+       grid[i][j] = 0; // Marking it 0 to identify as visited
        
        score += max({dfs(i-1,j,grid),dfs(i+1,j,grid),dfs(i,j-1,grid),dfs(i,j+1,grid)});
            
-       grid[i][j] = -grid[i][j]; // Making it positive while backtracking
+       grid[i][j] = temp; //Restore the value while backtracking
         
        return score;
     }
