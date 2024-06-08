24cb0efe48b6764c4fe02301f8c8526f83106b55
Arnab Maity <122111430+arnab18maity@users.noreply.github.com>
2024-06-08 23:09:37 +0530

    Time: 1222 ms (12.50%), Space: 25.5 MB (12.50%) - LeetHub

--- 3176-find-the-maximum-length-of-a-good-subsequence-i/3176-find-the-maximum-length-of-a-good-subsequence-i.cpp
+++ 3176-find-the-maximum-length-of-a-good-subsequence-i/3176-find-the-maximum-length-of-a-good-subsequence-i.cpp
@@ -23,9 +23,59 @@ public:
     }
     
     int maximumLength(vector<int>& nums, int k) {
-       int n = nums.size();
-       vector<vector<vector<int>>>dp(n,vector<vector<int>>(n+1,vector<int>(k+1,-1)));
+       // int n = nums.size();
+       // vector<vector<vector<int>>>dp(n,vector<vector<int>>(n+1,vector<int>(k+1,-1)));
+       // return helper(n-1,nums,k,n,dp); 
         
-       return helper(n-1,nums,k,n,dp); 
+        
+//         int n = nums.size(); 
+//         vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(n+1, vector<int>(k+1, 0)));
+        
+//         for (int ind = 1; ind <= n; ++ind) {
+//             for (int prev = 0; prev <= n; ++prev) {
+//                 for (int rem = 0; rem <= k; ++rem) {
+                    
+//                     int notTake = dp[ind-1][prev][rem];
+//                     int take = 0;
+                    
+//                     if(prev == n || nums[ind-1] == nums[prev]) {
+//                       take = 1 + dp[ind-1][ind-1][rem];
+//                     }
+
+//                     if(prev != n && nums[ind-1] != nums[prev] && rem > 0) {
+//                       take = 1 + dp[ind-1][ind-1][rem-1];
+//                     }   
+                    
+//                    dp[ind][prev][rem] = max(take,notTake); 
+//                 }
+//             }
+//         }
+        
+//         return dp[n][n][k];
+        
+     int n = nums.size();
+     vector<vector<int>> previous(n+1,vector<int>(k+1,0)), curr(n+1,vector<int>(k+1,0));
+      
+      for (int ind = 1; ind <= n; ++ind) {
+        for(int prev = 0; prev <= n; prev++) {
+            for(int rem = 0; rem <= k; rem++) {
+                    int notTake = previous[prev][rem];
+                    int take = 0;
+                    
+                    if(prev == n || nums[ind-1] == nums[prev]) {
+                      take = 1 + previous[ind-1][rem];
+                    }
+
+                    if(prev != n && nums[ind-1] != nums[prev] && rem > 0) {
+                      take = 1 + previous[ind-1][rem-1];
+                    }   
+                    
+                   curr[prev][rem] = max(take,notTake);
+            }
+        } 
+         previous = curr;  
+      }
+        
+        return previous[n][k];
     }
 };
