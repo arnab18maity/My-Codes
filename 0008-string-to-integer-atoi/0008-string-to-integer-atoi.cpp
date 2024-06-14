3b96d8bac737d6c6a5f1ef18891c94060f40addf
Arnab Maity <122111430+arnab18maity@users.noreply.github.com>
2024-06-14 22:38:55 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (36.88%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,30 +1,40 @@
 class Solution {
 public:
+    void helper(int ind, bool start, double &result, string &s) {
+       if(ind == s.size()) return;
+       if(!start && s[ind] != ' ' && s[ind] != '+' && s[ind] != '-' && !(s[ind] >= '0' && s[ind] <= '9')) return;
+       if(start && !(s[ind] >= '0' && s[ind] <= '9')) return;
+       
+        
+       if(!start && s[ind] == ' ') {
+           helper(ind+1,start,result,s);
+           return;
+       } 
+        
+       if(!start && s[ind] == '+') {
+          helper(ind+1,true,result,s);
+          return;
+       }
+    
+       if(!start && s[ind] == '-') {
+          helper(ind+1,true,result,s);
+          result *= -1;
+          return;
+       }
+      
+       int digit = s[ind] - '0';
+       result = (result*10 + digit);
+       helper(ind+1,true,result,s);
+    }
+    
     int myAtoi(string s) {
-        int len = s.size();
-        double num = 0;
-        int i = 0;
         
-        while(s[i] == ' '){
-            i++;
-        }
+      double result = 0;
+      helper(0,false,result,s);
+      
+      result = result > INT_MAX ? INT_MAX : result;
+      result = result < INT_MIN ? INT_MIN : result;
         
-        if(i == len) return 0;
-        
-        bool positive = (s[i] == '+');
-        bool negative = (s[i] == '-');
-        
-        if(positive == true || negative == true) i++;
-        
-        while(i < len && s[i] >= '0' && s[i] <= '9'){
-            num = num*10 + (s[i]-'0');
-            i++;
-        }
-        
-        num = negative ? -num : num;
-        num = (num > INT_MAX) ? INT_MAX : num;
-        num = (num < INT_MIN) ? INT_MIN : num;
-        
-        return int(num); 
+      return (int) result;
     }
 };
