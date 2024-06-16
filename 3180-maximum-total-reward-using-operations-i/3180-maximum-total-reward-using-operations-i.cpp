a8a26a20903baa82f403ef1e179caad2480a68ae
Arnab Maity <122111430+arnab18maity@users.noreply.github.com>
2024-06-16 15:27:00 +0530

    Time: 348 ms (75.90%), Space: 43.8 MB (80.51%) - LeetHub

--- 3180-maximum-total-reward-using-operations-i/3180-maximum-total-reward-using-operations-i.cpp
+++ 3180-maximum-total-reward-using-operations-i/3180-maximum-total-reward-using-operations-i.cpp
@@ -18,8 +18,23 @@ public:
       sort(rewardValues.begin(), rewardValues.end());
       int n = rewardValues.size();
         
-      vector<vector<int>> dp(n,vector<int>(4000,-1));
-      return helper(0,0,rewardValues,dp);
+      // vector<vector<int>> dp(n,vector<int>(4000,-1));
+      // return helper(0,0,rewardValues,dp);
+        
+      vector<int> curr(4000, 0), prev(4000, 0);
+        
+      for(int ind = n-1; ind >= 0; ind--) {
+        for(int curValue = 0; curValue < 4000; curValue++) {
+            int notTake = prev[curValue];        
+            int take = 0;
+            if(rewardValues[ind] > curValue) take = rewardValues[ind] + prev[rewardValues[ind] + curValue];
+            curr[curValue] = max(take,notTake);
+        }  
+          
+         prev = curr;
+      }
+        
+      return prev[0];   
               
     }
 };
