4b4ed6268267fb5ec645217d50785680851a7c1f
Arnab Maity <122111430+arnab18maity@users.noreply.github.com>
2024-09-25 00:17:09 +0530

    Time: 11 ms (63.03%), Space: 19.3 MB (40.60%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,32 +1,21 @@
 class Solution {
 public:
-    int jump(vector<int>& arr) {
-        int n = arr.size();
-        if(n == 1) return 0;
-        if(arr[0] == n-1) return 1; 
-        if(arr[0] == 0) return -1; 
-        vector<int> dp(n,0);
+    int jump(vector<int>& nums) {
+       int n = nums.size();
+       int l = 0, r = 0;
+       int jumps = 0;
         
-        for(int ind = n-2; ind >= 0 ; ind--) {
-          int ans = 1e8;
-            for(int i = 1; i <= arr[ind]; i++)
-            {
-              int res = 1e8;
-              
-              if((ind + i) < n) 
-                res = 1 + dp[ind+i];
-              else {
-                res = 1;
-                ans = min(ans,res);
-                break;
-              }
-              ans = min(ans,res);
-            }
-            
-            dp[ind] = ans;
-        }
+       while(r < n-1) {
+          int farthest = 0;
+          for(int i = l; i <= r; i++) {
+             farthest = max(farthest, i+nums[i]);
+          }
+           
+          l = r+1;
+          r = farthest;
+          jumps++;
+       }
         
-        return dp[0] == 1e8 ? -1 : dp[0];
-        
+        return jumps;
     }
 };
