e1a264c37eb2c243904c20ec036017abd9542c21
Arnab Maity <122111430+arnab18maity@users.noreply.github.com>
2024-10-02 13:57:30 +0530

    Time: 6 ms (14.87%), Space: 8.2 MB (74.71%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        vector<int> list;
         string res = "";
         int  i = 0;
         int count = 0;
@@ -9,25 +8,14 @@ public:
        while(i < s.size()) {
           if(s[i] == '(') {
               count++;
-              if(count == 1) list.push_back(i);
+              if(count != 1) res += s[i];
           }          
           else if(s[i] == ')') {
               count--;
-              if(count == 0) list.push_back(i);
+              if(count != 0) res += s[i];
           }
           
           i++;
-       }
-        
-        int j = 0;
-        
-       for(int i = 0; i < s.size(); i++) {
-          if(i == list[j]) {
-             j++;
-             continue;
-          } 
-           
-          res += s[i];
        }
         
        return res;
