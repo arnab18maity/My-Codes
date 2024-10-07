fe8d20df32e1fb6d432ba71903d626b001f4ea31
Arnab Maity <122111430+arnab18maity@users.noreply.github.com>
2024-10-07 21:28:45 +0530

    Time: 5 ms (81.94%), Space: 15.5 MB (57.20%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -11,43 +11,58 @@
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-       ListNode *oddHead = NULL, *oddTail = NULL;
-       ListNode *evenHead = NULL, *evenTail = NULL;
-       int index = 1;
+//        ListNode *oddHead = NULL, *oddTail = NULL;
+//        ListNode *evenHead = NULL, *evenTail = NULL;
+//        int index = 1;
          
-       ListNode *temp = head;
+//        ListNode *temp = head;
         
-       while(temp) {
-         if(index & 1) {
-               if(oddHead == NULL) {
-                 oddHead = temp;
-                 oddTail = temp;
-               } 
-               else{
-                 oddTail -> next = temp;
-                 oddTail = temp;
-               }
-         }
-         else{
-              if(evenHead == NULL) {
-                 evenHead = temp;
-                 evenTail = temp;
-              }
-              else{
-                  evenTail -> next = temp;
-                  evenTail = temp;
-              }
+//        while(temp) {
+//          if(index & 1) {
+//                if(oddHead == NULL) {
+//                  oddHead = temp;
+//                  oddTail = temp;
+//                } 
+//                else{
+//                  oddTail -> next = temp;
+//                  oddTail = temp;
+//                }
+//          }
+//          else{
+//               if(evenHead == NULL) {
+//                  evenHead = temp;
+//                  evenTail = temp;
+//               }
+//               else{
+//                   evenTail -> next = temp;
+//                   evenTail = temp;
+//               }
                
-         }
+//          }
            
-         index++;
-         temp = temp -> next;
+//          index++;
+//          temp = temp -> next;
+//        }
+        
+//        if(oddTail) oddTail -> next = evenHead;
+//        if(evenTail) evenTail -> next = NULL;
+        
+//        return oddHead;
+        
+       if(!head || !head -> next) return head;
+       ListNode *odd = head, *even = head -> next, *evenHead = head -> next;
+       
+       while(even && even -> next) {
+          odd -> next = odd -> next -> next;
+          even -> next = even -> next -> next;
+           
+          odd = odd -> next;
+          even = even -> next;
        }
         
-       if(oddTail) oddTail -> next = evenHead;
-       if(evenTail) evenTail -> next = NULL;
+       odd -> next = evenHead;
         
-       return oddHead;
+       return head;
        
     }
 };
