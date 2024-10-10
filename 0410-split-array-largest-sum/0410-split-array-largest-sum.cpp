d51b316eb337a12732b4fd8545f05b4155eefe18
Arnab Maity <122111430+arnab18maity@users.noreply.github.com>
2024-10-10 13:48:53 +0530

    Time: 4 ms (43.57%), Space: 9.4 MB (29.53%) - LeetHub

---
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+int helper(vector<int> &arr, int num) {
+   int count = 1, currPages = 0;
+
+   for(int i = 0; i < arr.size(); i++) {
+        if(currPages + arr[i] <= num) {
+          currPages += arr[i];
+        }
+        else {
+           count++;
+           currPages = arr[i];
+        }
+    } 
+
+    return count;
+}
+
+int findPages(vector<int>& arr, int n, int m) {
+    if(m > n) return -1;
+    int low = *max_element(arr.begin(), arr.end());
+    int high = accumulate(arr.begin(), arr.end(), 0); 
+
+    while(low <= high) {
+       int mid = low + (high-low)/2;
+       int res = helper(arr,mid);
+       
+        if(res > m) {
+          low = mid + 1;
+        }
+        else{
+          high = mid - 1;
+        }
+    }
+
+    return low;
+}
+
+    int splitArray(vector<int>& nums, int k) {
+       return findPages(nums, nums.size(), k);
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
