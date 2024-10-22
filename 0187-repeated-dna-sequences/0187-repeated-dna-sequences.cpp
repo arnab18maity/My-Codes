3da9049613bbaaa875b8c634d432cc07c78afc45
Arnab Maity <122111430+arnab18maity@users.noreply.github.com>
2024-10-22 12:22:38 +0530

    Time: 49 ms (46.15%), Space: 27.2 MB (42.68%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
@@ -4,18 +4,17 @@ public:
        int n = s.size();
        vector<string> ans;
        unordered_map<string,int> mp;
+        
        int l = 0, r = 9;
        
        while(r < n) {
           string temp = s.substr(l, 10);
           mp[temp]++;
+          if(mp[temp] == 2) ans.push_back(temp);
           l++;
           r++;
        }
         
-       for(auto it : mp) {
-          if(it.second > 1) ans.push_back(it.first);
-       }
         
        return ans;
     }
